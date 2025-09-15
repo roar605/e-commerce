2939fd4f7db42be42917641ae235018bc8ea672a
rohit <rksingh6050@gmail.com>
2025-09-15 14:55:00 +0530

    homepage structure done

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import '../componentStyles/Product.css'
+import { Link } from 'react-router-dom'
+import Rating from './Rating'
+
+
+function Product({ product }) {
+    const [rating,setRating]=useState(0);
+    const handleRatingChange=(newRating)=>{
+        setRating(rating)
+        
+    }
+
+    return (
+        <Link to={product._id} className='product_id'>
+            <div className="product-card">
+                <img src={product.image[0].url} alt={product.name} />
+                <div className="product-details">
+                    <h3 className="product-title">{product.name}</h3>
+                    <p className="product-price"><strong>Price</strong>{product.price}/-</p>
+                    <div className="rating-container">
+                        <Rating
+                        value={product.ratings}
+                        onRatingChange={handleRatingChange}
+                        disabled={true}
+                        />
+                    </div>
+                    <span className="productCardSpan">
+                        ({product.numOfReviews} Reviews)
+                    </span>
+                    <button className="add-to-cart">View Details</button>
+                </div>
+            </div>
+        </Link>
+
+    )
+}
+
+export default Product
---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import '../componentStyles/Rating.css'
+
+function Rating({ value, onRatingChange, disabled }) {
+    const [hoveredRating, setHoveredRating] = useState(0);
+    const [selectedRating, setSelectedRating] = useState(value || 0);
+
+    //handle star hover
+    const handleMouseEnter = (rating) => {
+        if (!disabled) {
+            setHoveredRating(rating)
+        }
+    }
+
+    //Mouse leave
+    const handleMouseLeave = () => {
+        if (!disabled) {
+            setHoveredRating(0);
+        }
+    }
+
+    //handle Click
+    const handleClick = (rating) => {
+        if (!disabled) {
+            setSelectedRating(rating)
+            if (onRatingChange) {
+                onRatingChange(rating)
+            }
+        }
+
+    }
+
+    //function to generate stars based on the selected rating
+    const generateStars = () => {
+        const stars = []
+        for (let i = 1; i <= 5; i++) {
+            const isFilled = i <= (hoveredRating || selectedRating);
+            stars.push(
+                <span key={i}
+                className={`star ${isFilled?'filled':'empty'}`}
+                onMouseEnter={()=>handleMouseEnter}
+                onMouseLeave={()=>handleMouseLeave}
+                style={{pointerEvents:disabled?'none':'auto'}}
+                >★</span>
+            )
+        }
+        return stars
+    }
+    return (
+        <div>
+            <div className="rating">{generateStars()}</div>
+        </div>
+    )
+}
+
+export default Rating
