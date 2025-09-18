be1b328383e1d9f3c53780774e0cd09ed7ccb301
rohit <rksingh6050@gmail.com>
2025-09-18 18:14:40 +0530

    updation

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,49 +1,66 @@
-import React from 'react'
-import '../componentStyles/Footer.css'
-import {Phone,Mail, GitHub, LinkedIn, Instagram, YouTube} from "@mui/icons-material"
+import React from "react";
+import "../componentStyles/Footer.css";
+import {
+  Phone,
+  Mail,
+  GitHub,
+  LinkedIn,
+  Instagram,
+  YouTube,
+} from "@mui/icons-material";
 
 function Footer() {
   return (
-    <footer className='footer'>
-        <div className='footer-container'>
-            {/* Section 1 */}
-            <div className="footer-section contact">
-                <h3>Contact Us</h3>
-                <p><Phone fontSize='small'/>Phone : +91 9305640458</p>
-                <p><Mail fontSize='small'/>Email : rksingh6050@gmail.com</p>
-            </div>
-
-            {/* Section 2 */}
-            <div className="footer-section social">
-                <h3>Follow me</h3>
-                <div className="social-links">
-                    <a href='' target='_blank'>
-                        <GitHub className='social-icon'/>
-                    </a>
-                    <a href='' target='_blank'>
-                        <LinkedIn className='social-icon'/>
-                    </a>
-                    <a href='' target='_blank'>
-                        <Instagram className='social-icon'/>
-                    </a>
-                    <a href='' target='_blank'>
-                        <YouTube className='social-icon'/>
-                    </a>
-                </div>
-            </div>
-
-            {/* Section 3 */}
-            <div className="footer-section about">
-                <h3>About</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam quod facere voluptatem corrupti. Pariatur eligendi, nostrum perspiciatis eaque minus sed, voluptas tenetur dicta excepturi deserunt corrupti asperiores voluptate, temporibus ut.</p>
-            </div>
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Section 1 */}
+        <div className="footer-section contact">
+          <h3>Contact Us</h3>
+          <p>
+            <Phone fontSize="small" />
+            Phone : +91 9305640458
+          </p>
+          <p>
+            <Mail fontSize="small" />
+            Email : rksingh6050@gmail.com
+          </p>
         </div>
-        <div className='footer-bottom'>
-            <p>&copy; 2025 RohitKrSingh . All Rights Reserved</p>
 
+        {/* Section 2 */}
+        <div className="footer-section social">
+          <h3>Follow me</h3>
+          <div className="social-links">
+            <a href="" target="_blank">
+              <GitHub className="social-icon" />
+            </a>
+            <a href="" target="_blank">
+              <LinkedIn className="social-icon" />
+            </a>
+            <a href="" target="_blank">
+              <Instagram className="social-icon" />
+            </a>
+            <a href="" target="_blank">
+              <YouTube className="social-icon" />
+            </a>
+          </div>
         </div>
+
+        {/* Section 3 */}
+        <div className="footer-section about">
+          <h3>About</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam
+            quod facere voluptatem corrupti. Pariatur eligendi, nostrum
+            perspiciatis eaque minus sed, voluptas tenetur dicta excepturi
+            deserunt corrupti asperiores voluptate, temporibus ut.
+          </p>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2025 - All Rights Reserved</p>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
