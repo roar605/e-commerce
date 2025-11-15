15dfa51b3217216a1e72965a7ac8be68392a71e2
Rohit Kumar Singh <107593487+roar605@users.noreply.github.com>
2025-11-15 13:45:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:8000'
+        target:' https://izetta-unilluminating-margarett.ngrok-free.dev'
       }
     }
   }
